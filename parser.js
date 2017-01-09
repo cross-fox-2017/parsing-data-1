568c12cf845209e248c00634f9038a914426e33d
Your Name <name@example.com>
2017-01-09 15:03:58 +0700

    Revisi Index Pada CSV Agar Tidak Berubah

--- parser.js
+++ parser.js
@@ -39,7 +39,7 @@ class PersonParser {
                     dataPerson.push(new Person(tampilData))
                 }).on('end', function(data) { // Untuk Menambah Data jika "end" diganti "data" prosesnya akan looping tidak akan berhenti
                     var tambahData = {
-                        id: "204",
+                        id: "201",
                         first_name: "Dila",
                         last_name: "Arista",
                         email: "diladilo@gmail.com",
@@ -48,7 +48,11 @@ class PersonParser {
                     }
                     parser.addPerson(new Person(tambahData))
                     for (var i = 0; i < dataPerson.length; i++) {
-                        temp += dataPerson[i].id + "," + dataPerson[i].first_name + "," + dataPerson[i].last_name + "," + dataPerson[i].email + "," + dataPerson[i].phone + "," + new Date(dataPerson[i].created_at) + "\n";
+                        if (i == 0) {
+                            temp += dataPerson[i].id + "," + dataPerson[i].first_name + "," + dataPerson[i].last_name + "," + dataPerson[i].email + "," + dataPerson[i].phone + "," + dataPerson[i].created_at + "\n";
+                        } else {
+                            temp += dataPerson[i].id + "," + dataPerson[i].first_name + "," + dataPerson[i].last_name + "," + dataPerson[i].email + "," + dataPerson[i].phone + "," + new Date(dataPerson[i].created_at) + "\n";
+                        }
                     }
                     console.log(temp);
                     parser.save(temp);
