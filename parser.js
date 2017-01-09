b8ca4d6455500487d03f7e0f89a3606ee81d4a54
Your Name <name@example.com>
2017-01-09 14:55:56 +0700

    Create Class Person

--- parser.js
+++ parser.js
@@ -4,3 +4,14 @@ var fs = require('fs')
 var csv = require('fast-csv')
 var dataPerson = [];
 var temp = "";
+
+class Person {
+    constructor(params) {
+        this.id = params['id']
+        this.first_name = params['first_name']
+        this.last_name = params['last_name']
+        this.email = params['email']
+        this.phone = params['phone']
+        this.created_at = params['created_at']
+    }
+}
