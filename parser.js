b32436359234788c13a3088ed117b7ef27c2d14f
Your Name <name@example.com>
2017-01-09 15:32:04 +0700

    Complete

--- parser.js
+++ parser.js
@@ -17,47 +17,42 @@ class Person {
 }
 
 class PersonParser {
-    constructor(file) {
-        this._file = file
-        this._people = null
+    constructor(csvFile) {
+        this._file = csvFile
     }
 
     get people() {
-        if (this._file == this._people) {
-            return this._people
-        } else {
-            fs.createReadStream(this._file).pipe(csv())
-                .on('data', function(data) { // Untuk Menampilkan Data
-                    var tampilData = {
-                        id: data[0],
-                        first_name: data[1],
-                        last_name: data[2],
-                        email: data[3],
-                        phone: data[4],
-                        created_at: data[5]
+        fs.createReadStream(this._file).pipe(csv())
+            .on('data', function(data) { // Untuk Menampilkan Data
+                var tampilData = {
+                    id: data[0],
+                    first_name: data[1],
+                    last_name: data[2],
+                    email: data[3],
+                    phone: data[4],
+                    created_at: data[5]
+                }
+                dataPerson.push(new Person(tampilData))
+            }).on('end', function(data) { // Untuk Menambah Data jika "end" diganti "data" prosesnya akan looping tidak akan berhenti
+                var tambahData = {
+                    id: "201",
+                    first_name: "Dila",
+                    last_name: "Arista",
+                    email: "diladilo@gmail.com",
+                    phone: "021-000033333",
+                    created_at: new Date()
+                }
+                parser.addPerson(new Person(tambahData))
+                for (var i = 0; i < dataPerson.length; i++) {
+                    if (i == 0) {
+                        temp += dataPerson[i].id + "," + dataPerson[i].first_name + "," + dataPerson[i].last_name + "," + dataPerson[i].email + "," + dataPerson[i].phone + "," + dataPerson[i].created_at + "\n";
+                    } else {
+                        temp += dataPerson[i].id + "," + dataPerson[i].first_name + "," + dataPerson[i].last_name + "," + dataPerson[i].email + "," + dataPerson[i].phone + "," + new Date(dataPerson[i].created_at) + "\n";
                     }
-                    dataPerson.push(new Person(tampilData))
-                }).on('end', function(data) { // Untuk Menambah Data jika "end" diganti "data" prosesnya akan looping tidak akan berhenti
-                    var tambahData = {
-                        id: "202",
-                        first_name: "Dila",
-                        last_name: "Arista",
-                        email: "diladilo@gmail.com",
-                        phone: "021-000033333",
-                        created_at: new Date()
-                    }
-                    parser.addPerson(new Person(tambahData))
-                    for (var i = 0; i < dataPerson.length; i++) {
-                        if (i == 0) {
-                            temp += dataPerson[i].id + "," + dataPerson[i].first_name + "," + dataPerson[i].last_name + "," + dataPerson[i].email + "," + dataPerson[i].phone + "," + dataPerson[i].created_at + "\n";
-                        } else {
-                            temp += dataPerson[i].id + "," + dataPerson[i].first_name + "," + dataPerson[i].last_name + "," + dataPerson[i].email + "," + dataPerson[i].phone + "," + new Date(dataPerson[i].created_at) + "\n";
-                        }
-                    }
-                    console.log(temp);
-                    parser.save(temp); //Memasukkan Data ke dalam csv, data awal tidak boleh dalam bentuk csv
-                })
-        }
+                }
+                console.log(temp);
+                parser.save(temp); //Memasukkan Data ke dalam csv, data awal tidak boleh dalam bentuk csv
+            })
     }
     addPerson(add) {
         dataPerson.push(add)
