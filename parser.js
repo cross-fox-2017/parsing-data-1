377c2a8f99046f780394367f0fe4ac795bde2bd0
didietsuryadi <didietsuryadi@gmail.com>
2017-01-19 20:10:36 +0700

    revisi write csv

--- parser.js
+++ parser.js
@@ -1,17 +1,18 @@
 "use strict"
 
 const fs = require('fs')
+let csv = require("fast-csv")
 
 class Person {
   // Look at the above CSV file
   // What attributes should a Person object have?
   constructor(id,first_name,last_name,email,phone,created_at) {
-    this._id = id
-    this._first_name = first_name
-    this._last_name = last_name
-    this._email = email
-    this._phone = phone
-    this._created_at = created_at
+    this.id = id
+    this.first_name = first_name
+    this.last_name = last_name
+    this.email = email
+    this.phone = phone
+    this.created_at = created_at
   }
 }
 
@@ -26,17 +27,10 @@ class PersonParser {
   get people() {
     // If we've already parsed the CSV file, don't parse it again
     // Remember: people is null by default
-    if (this._people)
-      return this._people
-
-    // We've never called people before, now parse the CSV file
-    // and return an Array of Person objects here
-    // Save the Array in the people instance variable.
     let peopleRead = fs.readFileSync(this._file, 'utf-8').split('\n')
     let peopleID = peopleRead.slice(1, peopleRead.length - 1)
 
     let peopleArray = [];
-
     for(let i = 0; i < peopleID.length; i++){
       peopleArray.push(peopleRead[i].split(','));
     }
@@ -55,21 +49,22 @@ class PersonParser {
   }
 
   addPerson(people = {}) {
-    this._data.push(people);
+    return this._data.push(people);
 
   }
 
-  // savePerson(){
-  //   let save = fs.createWriteStream(this._file);
-  //   csv.write(this._data).pipe(save);
-  // }
-
+  save() {
+    let save = fs.createWriteStream("newpeople.csv");
+    csv.write(this._data).pipe(save);
+  }
 }
-
 let parser = new PersonParser('people.csv')
-console.log(parser.people);
+
 let didit = new Person(302, 'Didit', 'Suryadi', 'didietsuryadi@gmail.com', '0816895056', `${new Date}`)
+console.log(parser.people);
 parser.addPerson(didit)
-// parser.save()
+console.log(parser._data);
+parser.save()
+
 
 console.log(`There are ${parser._data.length} people in the file '${parser._file}'.`)
